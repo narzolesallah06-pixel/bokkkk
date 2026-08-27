10d5ef43b55a7d6dc5a69c9bad98f0297f1734d5
narzolesallah06-pixel <narzolesallah06@gmail.com>
2026-08-27 14:46:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mysql = require("mysql2");
+app.use(cors());
+app.use(express.json());
 
 // Use environment variables or fallback defaults
 const pool = mysql.createPool({
